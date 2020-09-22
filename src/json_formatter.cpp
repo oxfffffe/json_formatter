c3e19d523a257e11b40aa2d72d812df480495b45
oxfffffe <59066701+oxfffffe@users.noreply.github.com>
2020-09-22 22:22:08 +0200

    Update json_formatter.cpp

--- src/json_formatter.cpp
+++ src/json_formatter.cpp
@@ -17,10 +17,18 @@ bool Json_formatter::file_has_json_ext(std::string filename)
 std::string Json_formatter::format() const {
   std::string formatted;
   uint8_t tabs = 0;
+  bool is_first_quote = true;
+  bool is_first_digit = true;
+  std::string end_of_color("\e[0m");
+  std::string grey("\e[38;5;240m");
+  std::string mint("\e[38;5;72m");
+  std::string purple("\e[38;5;176m");
   for (uint64_t i = 0; i < data.size(); i++) {
     switch (data[i]) {
       case static_cast<int>('{'): {
+        formatted.append(grey);
         formatted.push_back('{');
+        formatted.append(end_of_color);
         formatted.push_back('\n');
         ++tabs;
         for (uint8_t i = 0; i < tabs; i++) {
@@ -34,11 +42,15 @@ std::string Json_formatter::format() const {
           formatted.append(tab);
         }
         --tabs;
+        formatted.append(grey);
         formatted.push_back('}');
+        formatted.append(end_of_color);
         break;
       }
       case static_cast<int>('['): {
+        formatted.append(grey);
         formatted.push_back('[');
+        formatted.append(end_of_color);
         formatted.push_back('\n');
         ++tabs;
         for (uint8_t i = 0; i < tabs; i++) {
@@ -52,7 +64,9 @@ std::string Json_formatter::format() const {
           formatted.append(tab);
         }
         --tabs;
+        formatted.append(grey);
         formatted.push_back(']');
+        formatted.append(end_of_color);
         break;
       }
       case static_cast<int>(','): {
@@ -68,8 +82,40 @@ std::string Json_formatter::format() const {
         formatted.push_back(' ');
         break;
       }
+      case static_cast<int>('"'): {
+        if (is_first_quote) {
+          formatted.append(mint + "\"");
+        } else {
+          formatted.append("\"");
+          formatted.append(end_of_color);
+        }
+        is_first_quote = !is_first_quote;
+        break;
+      }
+      case static_cast<int>('0'):
+      case static_cast<int>('1'):
+      case static_cast<int>('2'):
+      case static_cast<int>('3'):
+      case static_cast<int>('4'):
+      case static_cast<int>('5'):
+      case static_cast<int>('6'):
+      case static_cast<int>('7'):
+      case static_cast<int>('8'):
+      case static_cast<int>('9'): {
+        if (is_first_quote) {
+          if (is_first_digit) {
+            formatted.append(purple);
+            formatted.push_back(data[i]);
+          } else {
+            formatted.append(end_of_color);
+            is_first_digit = !is_first_digit;
+          }
+          break;
+        }
+      }
       default: {
-        formatted.push_back(data[i]); break;
+        formatted.push_back(data[i]);
+        break;
       }
     }
   }
