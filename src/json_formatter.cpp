08e6b4843ffc080e735335a2a5085851c2a39645
oxfffffe <davydkar77@gmail.com>
2020-09-23 22:11:52 +0200

    final version

--- src/json_formatter.cpp
+++ src/json_formatter.cpp
@@ -1,66 +1,135 @@
 #include "json_formatter.hpp"
+#include <iostream>
 
-Json_formatter::Json_formatter(std::string filename) {
-    std::ifstream file_stream(filename);
-    std::string content((std::istreambuf_iterator<char>(file_stream))
-                       , std::istreambuf_iterator<char>());
-    file_content = content;
+Json_formatter::Json_formatter(const char* filename) {
+  assert(file_has_json_ext(filename) && "You specified non-JSON file, please try again.");
+  std::ifstream file_stream(filename);
+  std::string content((std::istreambuf_iterator<char>(file_stream)),
+                       std::istreambuf_iterator<char>());
+  data = content;
 }
 
-std::string Json_formatter::format() {
-    std::string formatted;
-    uint8_t count_of_tabs = 0;
-    const char* tab = "  ";
-    for (uint64_t i = 0; i < file_content.size(); i++) {
-        if (file_content[i] == '{') {                   //`{` case
-            formatted.push_back('{');
-            formatted.push_back('\n');
-            ++count_of_tabs;
-            for (uint8_t i = 0; i < count_of_tabs; i++) {
-                formatted.append(tab);
-            }
-        } else if (file_content[i] == '}') {            //`}` case
-            formatted.push_back('\n');
-            for (uint8_t i = 0; i < count_of_tabs - 1; i++) {
-                formatted.append(tab);
-            }
-          --count_of_tabs;
-            formatted.push_back('}');
-        } else if (file_content[i] == '[') {            //`[` case
-            formatted.push_back('[');
-            formatted.push_back('\n');
-          ++count_of_tabs;
-            for (uint8_t i = 0; i < count_of_tabs; i++) {
-                formatted.append(tab);
-            }
-        } else if (file_content[i] == ']') {            //`]` case
-            formatted.push_back('\n');
-            for (uint8_t i = 0; i < count_of_tabs - 1; i++) {
-                formatted.append(tab);
-            }
-          --count_of_tabs;
-            formatted.push_back(']');
-        } else if (file_content[i] == ',') {           //`,` case
-            formatted.push_back(',');
-            formatted.push_back('\n');
-            for (uint8_t i = 0; i < count_of_tabs; i++) {
-                formatted.append(tab);
-            }
-        } else {
-            formatted.push_back(file_content[i]);
-        }
+bool Json_formatter::file_has_json_ext(std::string filename) {
+  std::string json_ext = ".json";
+  return filename.substr(filename.size() - json_ext.size()) == json_ext;
+}
+
+void Json_formatter::append_brace(char brace, std::string& formatted, uint8_t& tabs, const std::string& color) const {
+  if (is_inside_quotes) {
+    formatted.push_back(brace);
+    return;
+  }
+  if (brace == '}' ||
+      brace == ']') {
+    formatted.push_back('\n');
+    for (uint8_t i = 0; i < tabs - 1; i++) {
+      formatted.append(tab);
     }
-    return formatted;
+    --tabs;
+    formatted.append(color);
+    formatted.push_back(brace);
+    formatted.append(end_of_color);
+  }
+  if (brace == '{' ||
+      brace == '[') {
+    formatted.append(color);
+    formatted.push_back('{');
+    formatted.append(end_of_color);
+    formatted.push_back('\n');
+    ++tabs;
+    for (uint8_t i = 0; i < tabs; i++) {
+      formatted.append(tab);
+    }
+  }
 }
 
+void Json_formatter::append_comma(std::string& text, uint8_t& tabs) const {
+  text.push_back(',');
+  if (!is_inside_quotes) {
+    text.push_back('\n');
+    for (uint8_t i = 0; i < tabs; i++) {
+      text.append(tab);
+    }
+  }
+}
 
+void Json_formatter::append_semicolon(std::string& text) const {
+  text.push_back(':');
+  if (!is_inside_quotes) {
+    text.push_back(' ');
+  }
+}
 
+void Json_formatter::append_quote(std::string& text) const {
+  if (!is_inside_quotes) {
+    text.append(mint + "\"");
+  } else {
+    text.append("\"");
+    text.append(end_of_color);
+  }
+  is_inside_quotes = !is_inside_quotes;
+}
 
+void Json_formatter::append_digit(std::string& text,  char digit) const {
+  if (!is_inside_quotes) {
+    text.append(purple + digit);
+    text.append(end_of_color);
+  } else {
+    text.push_back(digit);
+  }
+}
 
+std::string Json_formatter::format() const {
+  std::string formatted;
+  uint8_t tabs = 0;
+  for (uint64_t i = 0; i < data.size(); i++) {
+    switch (data[i]) {
+      case '{':
+        append_brace('{', formatted, tabs, grey);
+        break;
 
+      case '}':
+        append_brace('}', formatted, tabs, grey);
+        break;
 
+      case '[':
+        append_brace('[', formatted, tabs, grey);
+        break;
 
+      case ']':
+        append_brace(']', formatted, tabs, grey);
+        break;
 
+      case ',':
+        append_comma(formatted, tabs);
+        break;
 
+      case ':':
+        append_semicolon(formatted);
+        break;
 
+      case '"':
+        append_quote(formatted);
+        break;
+
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        append_digit(formatted, data[i]);
+        break;
+
+      default:
+        formatted.push_back(data[i]);
+        break;
+    }
+  }
+  return formatted;
+}
 
--- src/json_formatter.hpp
+++ src/json_formatter.hpp
@@ -1,18 +1,40 @@
-#ifndef JSON_FORMATTER_HPP
+#if not defined (JSON_FORMATTER_HPP)
 #define JSON_FORMATTER_HPP
 
-#include <fstream>
-#include <string>
-#include <streambuf>
+#include <fstream> //string included
+#include <cassert>
 
 class Json_formatter
 {
 public:
-    Json_formatter(std::string filename);
-    std::string format();
-private:
-    std::string file_content;
-};
+  Json_formatter() = delete;
+  Json_formatter(Json_formatter&) = delete;
+  Json_formatter(Json_formatter&&) = delete;
+  Json_formatter operator=(Json_formatter&) = delete;
+  Json_formatter(const char* filename);
+  std::string format() const;
 
+private:
+  std::string data;
+  const char* tab = "  ";
+  mutable bool is_inside_quotes = false;
+#if defined (__linux__)
+  std::string end_of_color = "\e[0m";
+  std::string grey = "\e[38;5;240m";
+  std::string mint = "\e[38;5;72m";
+  std::string purple = "\e[38;5;176m";
+#else
+  std::string end_of_color("");
+  std::string grey("");
+  std::string mint("");
+  std::string purple("");
+#endif
+  bool file_has_json_ext(std::string filename);
+  void append_brace(char brace, std::string& text, uint8_t& tabs, const std::string& color) const;
+  void append_comma(std::string& text, uint8_t& tabs) const;
+  void append_semicolon(std::string& text) const;
+  void append_quote(std::string& text) const;
+  void append_digit(std::string& text, char digit) const;
+};
 
 #endif // JSON_FORMATTER_HPP
