f0097eabea52bf8bcaf025dcb2a1a7bcf564a8e8
oxfffffe <59066701+oxfffffe@users.noreply.github.com>
2020-09-23 21:18:28 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,10 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-<<<<<<< HEAD
- if (argc != 2) {
-   throw std::invalid_argument
-   ("Invalid count of arguments, please add ONE json file as first parameter.");
- }
- Json_formatter formatter(argv[1]);
-  std::cout << formatter.format() << std::endl;
-=======
   if (argc != 2) {
     throw std::invalid_argument
     ("Invalid count of arguments, please add ONE json file as first parameter.");
   }
   Json_formatter formatter(argv[1]);
-  std::string formatted = formatter.format();
-  std::cout << formatted << std::endl;
->>>>>>> eec0e41897978819419a53e4be56d97b0c3b04a9
+  std::cout << formatter.format() << std::endl;
 }
